d0559ad2cfae23964e557c7e424d8646ec234843
Aditya <adityarao1999.ar@gmail.com>
2021-02-11 00:33:50 +0530

    inserted Assignment1

---
+++ Day2-Assignment1/ques1.js
@@ -0,0 +1,16 @@
+// since one is integer but JavaScript will try to make the  string into integer too
+console.log(1 + "2" + "2");
+
+// + or - operand in front of a string converts it to number.
+console.log(1 + +"2" + "2");
+// + or - operand in front of a string converts it to number.therefore it will become 1 - 1 + "2" = 02
+console.log(1 + -"1" + "2");
+
+//  + or - operand in front of a string converts it to number.1+1+"2" = 1 +"1" + "2" = 112
+console.log(+"1" + "1" + "2");
+
+// since A-B is not possible therefore NaN + "2" = NaN2
+console.log("A" - "B" + "2");
+
+// since A-B is not possible therefore NaN + 2 = NaN
+console.log("A" - "B" + 2);
